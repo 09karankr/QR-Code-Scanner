3f578c0708ad238330231eb7449d6c119f47cc57
Karan Kumar <officialkaran39@gmail.com>
2023-11-12 13:08:13 +0530

    worked on image analyzer

--- app/src/main/java/com/example/qrcodescanner/QrCodeAnalyzer.kt
+++ app/src/main/java/com/example/qrcodescanner/QrCodeAnalyzer.kt
@@ -3,6 +3,12 @@ package com.example.qrcodescanner
 import android.graphics.ImageFormat
 import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.BinaryBitmap
+import com.google.zxing.DecodeHintType
+import com.google.zxing.MultiFormatReader
+import com.google.zxing.PlanarYUVLuminanceSource
+import com.google.zxing.common.HybridBinarizer
 import java.nio.ByteBuffer
 
 class QrCodeAnalyzer(
@@ -20,6 +26,36 @@ class QrCodeAnalyzer(
     override fun analyze(image: ImageProxy) {
         if (image.format in supportedImageFormats){
             val bytes = image.planes.first().buffer.toByteArray()
+
+            val source = PlanarYUVLuminanceSource(
+                bytes,
+                image.width,
+                image.height,
+                 0,
+                0,
+                image.width,
+                image.height,
+                false
+            )
+            val binaryBmp = BinaryBitmap(HybridBinarizer(source))
+            try {
+                val result = MultiFormatReader().apply {
+                    setHints(
+                        mapOf(
+                            DecodeHintType.POSSIBLE_FORMATS to arrayListOf(
+
+                                BarcodeFormat.QR_CODE
+                            )
+                        )
+                    )
+                }.decode(binaryBmp)
+                onQrCodeScanned(result.text)
+
+            } catch (e: Exception ){
+                e.printStackTrace()
+            } finally {
+                image.close()
+            }
         }
 
     }
